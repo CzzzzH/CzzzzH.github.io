0c403b1df3a3d41b220c36f3622393a6d1c5ef5b
CzzzzH <morphling233@gmail.com>
2024-05-03 22:02:00 -0700

    Add MLTD

---
+++ src/publications.ts
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        category: 'Publications',
+        title: 'Temporally Stable Metropolis Light Transport Denoising using Recurrent Transformer Blocks',
+        image: '/Publications/MLTD.png',
+        authors: '<b>Chuhao Chen</b>, Yuze He, Tzu-Mao Li',
+        publisher: 'ACM Transactions on Graphics (Proceedings of <b>SIGGRAPH 2024</b>)',
+        buttons: [
+            {
+                icon: 'fa-solid fa-file-lines fa-2x',
+                text: 'Project Page',
+                link: '/MLTD/'
+            },
+            {
+                icon: 'fa-solid fa-file-pdf fa-2x',
+                text: 'Paper',
+                link: '/MLTD/MLTD.pdf'
+            },
+            {
+                icon: 'fa-solid fa-circle-play fa-2x',
+                text: 'Video',
+                videoSrc: '/MLTD/MLTD.mp4'
+            },
+            {
+                icon: 'fab fa-github fa-2x',
+                text: 'Code',
+                link: 'https://github.com/CzzzzH/MLTD'
+            }
+        ]
+    },
+]
